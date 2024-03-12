0de89b1534dcb0d61aed4ce194fdcd739f7a5bfc
Moin Alam <moinalam010@gmail.com>
2024-03-12 21:56:24 +0600

    +

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+
+
+// getCity("dhaka").then(data => {
+//     return getWeather(data.Key);
+// }).then(data => {
+//     console.log(data);
+// })    .catch(err => console.log(err));
--- js/forecast.js
+++ js/forecast.js
@@ -1,5 +1,24 @@
+
+
 const key = "99Ju6pHGNHme4LgEKO3f8yNgTeN88Cgs";
 
+// getting weather info
+const getWeather = async (id) => {
+
+
+    const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base+query);
+    const data = await response.json();  
+
+    return data[0];
+    
+
+}
+
+
+// getting city info
 const getCity = async (city) => {
 
     const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
@@ -8,7 +27,7 @@ const getCity = async (city) => {
     const response = await fetch(base+query);
     const data = await response.json();  
 
-    console.log(data)
+    return data[0];
 }
 
-getCity("dhaka");
+
