1d1cc2d19b48832de21c12963ad3ea0b8061bf4b
Moin Alam <moinalam010@gmail.com>
2024-03-14 15:13:35 +0600

    Final

--- js/app.js
+++ js/app.js
@@ -59,4 +59,14 @@ cityNameForm.addEventListener("submit", e =>{
         .catch(err => console.log(err));
 
 
+    // set local storage
+
+    localStorage.setItem("city" , city);
+
 })
+
+if(localStorage.getItem("city")){
+    updateCity(localStorage.getItem("city"))
+        .then(data => updateUI(data))
+        .catch( err => console.log(err));
+}
