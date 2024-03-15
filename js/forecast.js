3a0fea1c4d30c1228c90500c2f47601a937f02fb
Moin Alam <moinalam010@gmail.com>
2024-03-15 14:58:30 +0600

    +

--- js/forecast.js
+++ js/forecast.js
@@ -6,7 +6,7 @@ const key = "q8BU1sveaHA7EAYfN1c1t3PaHoqzaHG5";
 const getWeather = async (id) => {
 
 
-    const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+    const base = "https://dataservice.accuweather.com/currentconditions/v1/";
     const query = `${id}?apikey=${key}`;
 
     const response = await fetch(base+query);
@@ -21,7 +21,7 @@ const getWeather = async (id) => {
 // getting city info
 const getCity = async (city) => {
 
-    const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    const base = "https://dataservice.accuweather.com/locations/v1/cities/search";
     const query = `?apikey=${key}&q=${city}`;
 
     const response = await fetch(base+query);
